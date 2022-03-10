c67693227b782ce03e86c5f7e3ca00c7b3c93bcb
Laura <Laura.stabulniece@gmail.com>
2022-03-10 15:28:35 +0200

    copy-paste

---
+++ src/Components/Task.js
@@ -0,0 +1,49 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+
+import { useState } from "react";
+import { changeTaskStatus, deleteTask } from "../API/tasks";
+
+function Task(props) {
+    const { task, loadTasks } = props
+    const [taskChanging, setTaskChanging] = useState(false)
+
+    const changeIsCompletedState = async () => {
+        setTaskChanging(true)
+        await changeTaskStatus(task._id, !task.isCompleted, task.title)
+        await loadTasks()
+    }
+
+    const deleteTaskAction = async () => {
+        setTaskChanging(true)
+        await deleteTask(task._id)
+        await loadTasks()
+    }
+
+
+    let deleteBtn = ''
+    if (task.isCompleted) {
+        deleteBtn = <button 
+                className="btn btn-danger"
+                disabled={taskChanging}
+                onClick={deleteTaskAction}
+            >
+                <FontAwesomeIcon icon={faTrashCan} />
+            </button>
+    }
+
+    return (
+        <li className="list-group-item">
+            <input 
+                type="checkbox" 
+                checked={task.isCompleted} 
+                disabled={taskChanging}
+                onChange={changeIsCompletedState}
+            />
+            <span>{task.title}</span>
+            {deleteBtn}
+        </li>
+    );
+}
+
+export default Task;
